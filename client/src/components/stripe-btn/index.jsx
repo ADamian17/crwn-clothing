e4d24ceee36578d759f23893083c26fb201f2d3e
ADamian17 <adonisdesuvikdo@gmail.com>
2021-09-23 10:58:12 -0700

    added payment

--- client/src/components/stripe-btn/index.jsx
+++ client/src/components/stripe-btn/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from 'axios';
+
+
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutBtn = ({ price }) => {
 
   const priceForStripe = price * 100;
+
+  const handleToken = (token) => {
+    axios.post({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then((res) => console.log({ res }))
+      .catch((error) => console.log(error))
+  }
 
   return (
     <StripeCheckout
@@ -14,7 +30,7 @@ const StripeCheckoutBtn = ({ price }) => {
       image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlZvH6wqmQ5GhCyyhFwZHYfXwGxH7aAvmG4w&usqp=CAU"
       description={`Your Total is $${price}`}
       panelLabel="Pay now"
-      token={(token) => console.log(token)}
+      token={handleToken}
       stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY} />
   )
 };
--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require("express");
 const path = require("path"); 
 
 if (process.env.NODE_ENV === 'production') require("dotenv").config()
-const app = express();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const PORT = process.env.PORT || 4000;
+
+const app = express();
 
 /* == Middleware == */
 app.use(express.json())
@@ -17,6 +20,22 @@ if(process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) => res.sendFile( path.join(__dirname, 'client/build', 'index.html') ))
 }
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  }
+
+  stripe.charges.create(body, (error, stripeRes) => {
+    if(error) return res.status(500).json({error});
+
+    res.status(200).json({
+      stripeRes
+    })
+  })
+})
 
 app.listen(PORT, () =>
   console.log(`Listening for client requests on port ${PORT}`)
