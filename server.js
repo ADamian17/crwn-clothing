3d13ad6f612f8e51c90325f3e408ed0f6d1749b3
ADamian17 <adonisdesuvikdo@gmail.com>
2021-09-22 22:10:52 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-/* External Modules */
+/* == External Modules == */
 const express = require("express");
-const cors = require("cors");
 const path = require("path"); 
 
-
+if (process.env.NODE_ENV === 'production') require("dotenv").config()
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-if (process.env.NODE_ENV === 'production') require("dotenv").config()
+/* == Middleware == */
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+/* == Routes == */
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => res.sendFile( path.join(__dirname, 'client/build', 'index.html') ))
+}
 
 app.listen(PORT, () =>
   console.log(`Listening for client requests on port ${PORT}`)
